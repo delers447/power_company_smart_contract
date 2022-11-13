c0cb216a2b1642fb50fc2008f82ca7b7ab26dfa9
Nicholas <84189932+Nawaugh03@users.noreply.github.com>
2022-11-13 18:51:45 -0500

    Update Customer.sol

--- Customer.sol
+++ Customer.sol
@@ -4,8 +4,21 @@ pragma solidity >=0.7.0 <0.9.0;
 contract Customer{
 
 	address private customer;
+	string public Complaint
+	string private Fname;
+	string private Lname;
 	uint private money;
 	
+	constructor() public {
+	
+	}
+	function MakePayment() returns(uint m){
+		return m;
+	}
+	function WriteComplaint(string M)public view returns (string message){
+		Complaint=M;
+		return M;
+	}
 	
 
 }
