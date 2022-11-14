159eddabe669f71e2a47f421bfd41962c50bd114
Nicholas <84189932+Nawaugh03@users.noreply.github.com>
2022-11-14 08:50:23 -0500

    Update Customer.sol
    Working on storing Fname

--- Customer.sol
+++ Customer.sol
@@ -1,13 +1,25 @@
-pragma solditity ^0.8.0
+pragma solidity ^0.8.0;
 
 contract Argument{
 
 }
 
 contract Payment{
-
+    struct C_info {
+        string Fname;
+        string Lname;
+        string location;
+        uint payments;
+    }
+    mapping(uint=>C_info) public Customers;
+    function StoreFirstName(string F) public {//Store First Name
+       Customers[index].Fname = F;//Error? Undeclared Identifier
+    }
+    function UpdatePayments(uint C) public {
+        
+    }
 }
 
 contract Power{
-    
+
 }
