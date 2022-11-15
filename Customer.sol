412adb02ce82e2daf7e7bbaa1750c21e0f7d73b7
Nawaugh03 <nickgithub03@gmail.com>
2022-11-15 18:03:33 -0500

    Good so far

--- Customer.sol
+++ Customer.sol
@@ -1,23 +1,38 @@
 pragma solidity ^0.8.0;
 
-contract Argument{
-
-}
-
 contract Payment{
     struct C_info {
+        mapping (uint=> address) public balances
         string Fname;
         string Lname;
         string location;
-        uint payments;
+        address payable department;
     }
-    
-    function StoreFirstName(string F) public {//Store First Name
-    	C_info.Fname=F;   
+    C_info public customers;
+    function StoreFirstName(string memory F) public {
+    	customers.Fname = F;   
+    }
+    function StoreLastName(string memory L) public {
+        customers.Lname=L;
+    }
+    function StoreLocation(string memory A) public {
+        customers.location=A;
+    }
+    function getFirstName() public view returns (string memory){
+        return customers.Fname;
+    }
+    function getLastName() public view returns (string memory){
+        return customers.Lname;
+    }
+    function getLocation() public view returns (string memory){
+        return customers.location;
     }
     function UpdatePayments(uint C) public {
-        
+
     }
+}
+contract argument{
+
 }
 
 contract Power{
