27a2784de46bb358e70c769f19114f47f7c39098
Nawaugh03 <nickgithub03@gmail.com>
2022-11-19 20:55:37 -0500

    Starting to work on services

---
+++ Service.sol
@@ -0,0 +1,20 @@
+Pragma solidity 0.5.16
+
+contract Service{
+	struct Power_providers{
+	}
+	struct Maintenance_provid{
+	
+	}
+	struct Government_People{
+	
+	}
+
+	function Make_service_agreement(){
+	
+	}
+
+	function Complete_service_agreement(){
+
+	}
+}
