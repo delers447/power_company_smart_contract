1b1dc12d17f19eddea1fe9aed7d316051f580987
Moonfox8008 <118021059+Moonfox8008@users.noreply.github.com>
2022-12-14 12:15:06 -0500

    Update Complaints.sol
    Text me if there more problems

--- Complaints.sol
+++ Complaints.sol
@@ -2,7 +2,7 @@ pragma solidity 0.5.16;
 
 contract Complaints {
    
-   
+    uint complaintindex = 0;                // Indexs of the complaints
     uint public no_of_complaints;           // Counts the number of complaints made
     address  _government_agency;            // Stores the address of the goverment agency
     
@@ -20,7 +20,7 @@ contract Complaints {
 
     modifier only_government_agency(){       // Makes sure only a goverment agency can use a function
         require(msg.sender == _government_agency, "You aren't authorize");
-        require (complaints_by_number[no_of_complaints].been_judged == false , "Already been judge") ;
+        require (complaints_by_number[complaintindex].been_judged == false , "Already been judge") ;
        _; 
     }
 
@@ -32,19 +32,20 @@ contract Complaints {
     function make_ComplaintsByPassing(string memory message) public {       
         // Creates a complaint and stores it on the complaint array along with the time of the complaint and the address of the complainer
         // Message essentaily a call in that allows the users to write a string into the function in the compl string
-
+        complaintindex++;
         no_of_complaints++;
-        complaints_by_number[no_of_complaints] = complaint (message,"none",false,now,0,msg.sender,address(0));
+        complaints_by_number[complaintindex] = complaint (message,"none",false,now,0,msg.sender,address(0));
     }
 
       function make_JudgementByPassing(string memory message) public only_government_agency  {   
           // Adds a judgement to a complaint array along with the time of the judgment a true value to the been judged boolean and the address of the judge.
           // Message essentaily a call in that allows the users to write a string into the function in the judge string
           // Applies the modifier only_government_agency which prevents address not registered as a goverment agency from using this function
-        complaints_by_number[no_of_complaints].been_judged = true;
-        complaints_by_number[no_of_complaints].time_of_judgement = now;
-        complaints_by_number[no_of_complaints].judge = message;
-        complaints_by_number[no_of_complaints]._government_agency = msg.sender;
+        require(complaintindex>0);
+        complaints_by_number[index].been_judged = true;
+        complaints_by_number[index].time_of_judgement = now;
+        complaints_by_number[index].judge = message;
+        complaints_by_number[index]._government_agency = msg.sender;
     }
     
 
